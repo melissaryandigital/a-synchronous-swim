e30f36ec517a37e42568682ae5366ffe0b294ccc
jrudio <justinjrudio@gmail.com>
2020-12-15 20:05:14 -0800

    background image progress

--- server/js/httpHandler.js
+++ server/js/httpHandler.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const url  = require('url');
 const path = require('path');
 const headers = require('./cors');
 const multipart = require('./multipartUtils');
@@ -18,17 +19,61 @@ module.exports.initialize = (queue) => {
 module.exports.router = (req, res, next = ()=>{}) => {
   // console.log('Serving request type ' + req.method + ' for url ' + req.url);
 
-  if (req.method === 'GET') {
-    res.writeHead(200, headers);
+  // Find the requested path name
+  var url_parts = url.parse(req.url);
+  var pathName = url_parts.pathname;
+  // 'spec/missing.jpg'
+  console.log(pathName);
+  switch (pathName) {
+    case '/background.jpg':
+      if (req.method === 'GET') {
+        // read image from disk
+        fs.readFile(path.join('.', pathName), 'utf8', (err, data) => {
+          if (err) {
+            // if it doesn't exist return 404 status code
+            res.writeHead(404, headers);
+            return;
+          }
 
-    res.write(messageQueue.dequeue() || 'up', 'utf-8');
-    res.end();
-    next();
+          res.writeHead(200, headers);
+          // return image and 200 status code
+          res.write(data, 'utf8');
+          res.end();
+          next();
+        });
+
+        // res.writeHead(200, headers);
+        // res.end();
+        // next();
+        return;
+      }
+
+    case '/spec/missing.jpg':
+      res.writeHead(404, headers);
+      res.end();
+      next();
+    case '/':
+      if (req.method === 'OPTIONS') {
+        res.writeHead(200, headers);
+        res.end();
+        next();
+        return;
+      }
+
+      if (req.method === 'GET') {
+        res.writeHead(200, headers);
+
+        res.write(messageQueue.dequeue() || 'up', 'utf-8');
+        res.end();
+        next();
+        return;
+      }
+    default:
+      // 404
+      res.writeHead(404, headers);
+      res.end();
+      next();
   }
-
-  res.writeHead(200, headers);
-  res.end();
-  next(); // invoke next() at the end of a request to help with testing!
 };
 
 
